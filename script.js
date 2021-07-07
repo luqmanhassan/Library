ee51147dbdadfc7a6a23eac1e5c3a94115da0831
Luqman Hassan <reallookmanlook@gmail.com>
2021-07-07 15:31:45 -0700

    New

---
+++ script.js
@@ -0,0 +1,15 @@
+let myLibrary = [];
+
+function Book() {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return this.title + " by " + this.author + ", " + this.pages + ", " + this.read;
+    }
+}
+
+function addBookToLibrary() {
+
+}
